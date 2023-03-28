dc3b146dcb1b0138f286677be4168dfb8bcc2fd4
marco cardenas <mardcart.9@gmail.com>
2023-03-28 20:20:51 -0300

    se crea el componente searchform

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
 import 'bulma/css/bulma.css'
 import { Title } from './Components/Title';
+import { SearchForm } from './Components/SearchForms';
 
 
 function App() {
   return (
     <div className="App">
       <Title>buscador de peliculas</Title>
+      <div className='SearchForm-wrapper'>
+      <SearchForm/>
+      </div>
     </div>
   );
 }
