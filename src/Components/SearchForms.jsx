8c1a83c2e72782cc005a46749404b48a161e2a26
marco cardenas <mardcart.9@gmail.com>
2023-03-28 22:37:39 -0300

    se conecta a una api

--- src/Components/SearchForms.jsx
+++ src/Components/SearchForms.jsx
@@ -1,5 +1,8 @@
 import { Component } from "react";
 
+const clave = '3a124f3e';
+
+
 export class SearchForm extends Component{
     state = {
         inputMovie: ''
@@ -10,7 +13,14 @@ export class SearchForm extends Component{
     }
     _handleSubmit=(e)=>{
         e.preventDefault()
-        alert(this.state.inputMovie)
+//        alert(this.state.inputMovie)
+        const {inputMovie} = this.state
+        fetch(`http://www.omdbapi.com/?apikey=${clave}&s=${inputMovie}`)
+        .then(res => res.json())
+        .then(results => {
+            console.log(results)
+        })
+
     }
 
     render(){
